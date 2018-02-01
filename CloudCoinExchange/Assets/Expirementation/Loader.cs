6e8ef01caae9cd40f408633c359a413c7807d63a
ChicoHoloDev <preston@chicoholo.com>
2018-02-01 12:27:26 -0800

    initial commit
    upload to repo

---
+++ CloudCoinExchange/Assets/Expirementation/Loader.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Loader : MonoBehaviour {
+
+    private Vector3 oldPosition;
+    private Quaternion oldRotation;
+    private Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags  & (~Hand.AttachmentFlags.DetachOthers);
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnHandHoverBegin(Hand hand)
+    {
+
+    }
+
+    //-------------------------------------------------
+    // Called when a Hand stops hovering over this object
+    //-------------------------------------------------
+    private void OnHandHoverEnd(Hand hand)
+    {
+
+    }
+
+
+    //-------------------------------------------------
+    // Called every Update() while a Hand is hovering over this object
+    //-------------------------------------------------
+    private void HandHoverUpdate(Hand hand)
+    {
+        if (hand.GetStandardInteractionButtonDown() || ((hand.controller != null) && hand.controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_Grip)))
+        {
+            if (hand.currentAttachedObject != gameObject)
+            {
+                // Save our position/rotation so that we can restore it when we detach
+                oldPosition = transform.position;
+                oldRotation = transform.rotation;
+
+                // Call this to continue receiving HandHoverUpdate messages,
+                // and prevent the hand from hovering over anything else
+                hand.HoverLock(GetComponent<Interactable>());
+
+                // Attach this object to the hand
+                hand.AttachObject(gameObject, attachmentFlags);
+            }
+            else if(!hand.GetStandardInteractionButton())
+            {
+                // Detach this object from the hand
+                hand.DetachObject(gameObject);
+
+                // Call this to undo HoverLock
+                hand.HoverUnlock(GetComponent<Interactable>());
+
+                // Restore position/rotation
+                transform.position = oldPosition;
+                transform.rotation = oldRotation;
+            }
+        }
+    }
+
+
+    //-------------------------------------------------
+    // Called when this GameObject becomes attached to the hand
+    //-------------------------------------------------
+    private void OnAttachedToHand(Hand hand)
+    {
+
+        
+    }
+
+
+    //-------------------------------------------------
+    // Called when this GameObject is detached from the hand
+    //-------------------------------------------------
+    private void OnDetachedFromHand(Hand hand)
+    {
+
+    }
+
+
+    //-------------------------------------------------
+    // Called every Update() while this GameObject is attached to the hand
+    //-------------------------------------------------
+    private void HandAttachedUpdate(Hand hand)
+    {
+
+    }
+
+
+    //-------------------------------------------------
+    // Called when this attached GameObject becomes the primary attached object
+    //-------------------------------------------------
+    private void OnHandFocusAcquired(Hand hand)
+    {
+    }
+
+
+    //-------------------------------------------------
+    // Called when another attached GameObject becomes the primary attached object
+    //-------------------------------------------------
+    private void OnHandFocusLost(Hand hand)
+    {
+    }
+}
